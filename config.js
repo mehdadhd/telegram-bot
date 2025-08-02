b2b7c6da59246d21b9768f8dd7759356ffe58c67
mehdadhd <33844722+mehdadhd@users.noreply.github.com>
2025-08-02 11:56:02 +0430

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,12 @@
 module.exports = {
-    BOT_TOKEN: "7592719498:AAF1-bj_rlVQrhsTJkNnmAHUnerLDLohYkI", // توکن ربات
-    CHANNEL_USERNAME: "@ahngmhdd", // نام کانال
-    BASE_COINS: ["bitcoin", "notcoin", "ethereum", "the-open-network", "solana", "dogecoin"],
-  };
+  BOT_TOKEN: "7592719498:AAF1-bj_rlVQrhsTJkNnmAHUnerLDLohYkI", // توکن ربات
+  CHANNEL_USERNAME: "@begushhh", // نام کانال
+  BASE_COINS: [
+    "bitcoin",
+    "notcoin",
+    "ethereum",
+    "the-open-network",
+    "solana",
+    "dogecoin",
+  ],
+};
